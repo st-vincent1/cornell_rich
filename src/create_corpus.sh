8394ce996910aee58d45743c353f8a217d20be23
Sebastian Vincent <19303946+st-vincent1@users.noreply.github.com>
2024-05-15 08:28:24 +0100

    Update create_corpus.sh

--- src/create_corpus.sh
+++ src/create_corpus.sh
@@ -3,7 +3,6 @@
 wget http://www.mpi-sws.org/~cristian/data/cornell_movie_dialogs_corpus.zip
 
 unzip cornell_movie_dialogs_corpus.zip
-rm -rf __MACOSX
 
 mv "cornell movie-dialogs corpus" cornell_movie_dialogs_corpus
 rm cornell_movie_dialogs_corpus.zip
